efede833f19ade1e112a0ec61c2070a676baf4bb
Onuegbu Udochukwu <onuegbuudochukwu6@gmail.com>
2025-07-14 01:38:40 +0100

    Market Summary Initial Folder Structure

---
+++ src/main/java/com/codewithudo/quidaxmarketsummary/QuidaxMarketSummaryApplication.java
@@ -0,0 +1,13 @@
+package com.codewithudo.quidaxmarketsummary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuidaxMarketSummaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuidaxMarketSummaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codewithudo/quidaxmarketsummary/QuidaxMarketSummaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithudo.quidaxmarketsummary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuidaxMarketSummaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
